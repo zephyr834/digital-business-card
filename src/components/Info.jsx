1cc00ed42cbedc338581f11d95aa3be3b2dd985a
Cory <lotsoftrash834@gmail.com>
2025-02-20 23:51:32 -0800

    Got FontAwesome to work and used in Info.

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,13 +1,18 @@
-
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Info() {
     return (
         <>
             <h1>Cory Eighan</h1>
             <p>Software Engineer</p>
-            <Button className="btn-email">Email</Button>
-            <Button className="btn-email">LinkedIn</Button>
+            <button className="btn-email">
+                <FontAwesomeIcon icon="fa-solid fa-envelope" />
+                Email
+            </button>
+            <button className="btn-email">
+                <img src="" className="email-icon" />
+                LinkedIn
+            </button>
         </>
     )
 }
