22c8e36840a58a7d3c6b6ebe4ecb484c68c2ffca
Cory <lotsoftrash834@gmail.com>
2025-02-21 19:43:14 -0800

    Adding styling

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,18 +1,22 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "./../styles/Info.css"
 
 export default function Info() {
     return (
-        <>
+        <div className="content">
+            <img src="" className="profile--pic" />
             <h1>Cory Eighan</h1>
             <p>Software Engineer</p>
-            <button className="btn-email">
-                <FontAwesomeIcon icon="fa-solid fa-envelope" />
-                Email
-            </button>
-            <button className="btn-email">
-                <img src="" className="email-icon" />
-                LinkedIn
-            </button>
-        </>
+            <div className="info--links" >
+                <button className="btn--email">
+                    <FontAwesomeIcon icon="fa-solid fa-envelope" />
+                    Email
+                </button>
+                <button className="btn--linkedin">
+                    <FontAwesomeIcon icon="fa-brands fa-linkedin" />
+                    LinkedIn
+                </button>
+            </div>
+        </div>
     )
 }
